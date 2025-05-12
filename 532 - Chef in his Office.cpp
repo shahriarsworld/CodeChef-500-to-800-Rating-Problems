9f3416d93c5567b68f28d71e097b72e9a6c00279
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-05-12 23:53:00 +0600

    Create 532 - Chef in his Office.cpp

---
+++ 532 - Chef in his Office.cpp
@@ -0,0 +1,27 @@
+// Recently Chef joined a new company. In this company, the employees have to work for 
+// X
+// X hours each day from Monday to Thursday. Also, in this company, Friday is called Chill Day — employees only have to work for 
+// Y
+// Y hours 
+// (
+// Y
+// <
+// X
+// )
+// (Y<X) on Friday. Saturdays and Sundays are holidays.
+
+// Determine the total number of working hours in one week.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int count,x,y;
+	cin>> count;
+	for (int i = 0; i < count; i++) {
+	    cin>> x >> y;
+	    std::cout << x + x + x + x + y << std::endl;
+	}
+
+}
