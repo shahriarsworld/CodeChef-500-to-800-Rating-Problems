ecf9671c152c2197ed1a062e3071219dca36873d
Md Shahriar Hossain <113468427+shahriarsworld@users.noreply.github.com>
2025-06-23 23:59:33 +0600

    Create 573 - The Three Topics.cpp

---
+++ 573 - The Three Topics.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,c,x;
+	std::cin >> a >> b >> c >> x;
+	
+	if((a==x) || (b==x) || (c==x)){
+	    std::cout << "Yes" << std::endl;
+	} else{
+	    std::cout << "No" << std::endl;
+	}
+
+}
